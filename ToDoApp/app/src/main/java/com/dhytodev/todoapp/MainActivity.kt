918b2b9412167bea948108524486bdbc6b9d6974
Fordyta Abubakar <dhytodev@gmail.com>
2019-12-21 12:20:10 +0800

    insert task and observe tasks

--- ToDoApp/app/src/main/java/com/dhytodev/todoapp/MainActivity.kt
+++ ToDoApp/app/src/main/java/com/dhytodev/todoapp/MainActivity.kt
@@ -3,40 +3,37 @@ package com.dhytodev.todoapp
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var taskDatabase: TaskDatabase
+    private val taskViewModel: TaskViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        taskDatabase = TaskDatabase.getInstance(this)
-
         insertTasks()
-        showTasks()
+        observeTasks()
     }
-
-    private fun addNewTask(task: Task): Long = taskDatabase.taskDao().insertTask(task)
-
-    private fun getAllTasks(): List<Task> = taskDatabase.taskDao().getTasks()
 
     private fun insertTasks() {
-        val task = Task(1, "Belajar Android")
-        val task2 = Task(2, "Belajar Git", true)
+        val task1 = Task(1, "Belajar Android")
+        val task2 = Task(2, "Belajar Java", true)
 
-        addNewTask(task)
-        addNewTask(task2)
+        taskViewModel.addNewTask(task1)
+        taskViewModel.addNewTask(task2)
+        
     }
 
-    private fun showTasks() {
-        val tasks = getAllTasks()
-
-        for (todo in tasks) {
-            Log.d("Task Name", todo.task)
-            Log.d("Task Completed", todo.completed.toString())
-        }
+    private fun observeTasks() {
+        taskViewModel.getAllTasks().observe(this, Observer {
+            if(it.isNotEmpty()) {
+                it.map {
+                    Log.d("tasks", it.toString())
+                }
+            }
+        })
     }
-
 }
