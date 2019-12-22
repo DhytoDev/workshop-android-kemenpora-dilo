97416e9409c1e41a55bf4b903bdda3952bd085b7
Fordyta Abubakar <dhytodev@gmail.com>
2019-12-22 13:25:20 +0800

    upgrade database version

--- ToDoApp/app/src/main/java/com/dhytodev/todoapp/TaskDatabase.kt
+++ ToDoApp/app/src/main/java/com/dhytodev/todoapp/TaskDatabase.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 @Database(
-    entities = [Task::class], version = 1,
+    entities = [Task::class], version = 2,
     exportSchema = false
 )
 abstract class TaskDatabase : RoomDatabase() {
@@ -17,13 +19,18 @@ abstract class TaskDatabase : RoomDatabase() {
         private var INSTANCE: TaskDatabase? = null
         private val lock = Any()
 
+        val migration_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {}
+        }
+
         fun getInstance(context: Context): TaskDatabase {
             if (INSTANCE == null) {
                 synchronized(lock) {
                     INSTANCE = Room.databaseBuilder(
                         context.applicationContext,
                         TaskDatabase::class.java, "todoapp.db"
-                    ).allowMainThreadQueries()
+                    ).addMigrations(migration_1_2)
+                        .allowMainThreadQueries()
                         .build()
                 }
                 return INSTANCE as TaskDatabase
