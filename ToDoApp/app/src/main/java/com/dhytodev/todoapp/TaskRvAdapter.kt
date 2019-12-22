c0d6cd5ff57d53bbdbed6830efe3244720a507c6
Fordyta Abubakar <dhytodev@gmail.com>
2019-12-22 13:01:40 +0800

    create Task Rv Adapter

---
+++ ToDoApp/app/src/main/java/com/dhytodev/todoapp/TaskRvAdapter.kt
@@ -0,0 +1,42 @@
+package com.dhytodev.todoapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.layout_task_item.view.*
+
+/**
+ * Created by DhytoDev on 2019-12-22.
+ * Email : dhytodev@gmail.com
+ */
+class TaskRvAdapter : RecyclerView.Adapter<TaskRvAdapter.TaskViewHolder>() {
+
+    val listTasks = listOf<Task>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.layout_task_item, parent, false)
+
+        return TaskViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = listTasks.size
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        val task = listTasks[position]
+
+        holder.bindView(task)
+    }
+
+    inner class TaskViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+
+        fun bindView(task: Task) {
+            itemView.apply {
+                textTaskName.text = task.task
+                checkboxComplete.isChecked = task.completed
+            }
+        }
+    }
+}
